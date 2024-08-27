eb706f6a1866094942699fc44d62556f48f17680
GeorgeHanna <georgehanna1993@gmail.com>
2024-08-27 13:45:54 +0300

    new file:   catalog.html 	modified:   index.html 	new file:   item1.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  // Smooth scrolling for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 
-    // Add any additional JavaScript animations or interactions here
+  // Hamburger menu toggle for mobile view
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const navbarMenu = document.getElementById("menu");
+
+  hamburgerMenu.addEventListener("click", function () {
+    navbarMenu.classList.toggle("show");
+  });
 });
